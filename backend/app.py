df58831a3a62249ccea61c47fbf59f13853f3a48
Amir <amirbrahamm@gmail.com>
2021-08-08 21:34:55 +0100

    cloudflare ftw

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+from bs4.builder import TreeBuilder
 from api import LibgenSearchModified
 from flask import Flask, jsonify
 
@@ -5,13 +6,11 @@ app = Flask(__name__)
 
 s = LibgenSearchModified()
 
-''' 
- '''
-
 
 @app.route("/<title>", methods=["GET"])
 def hello_world(title):
-    results = s.search_title(title)
+    results = s.search_title_filtered(
+        title, {"Extension": "epub"}, exact_match=True)
     for i in range(len(results)):
         results[i]["Image"] = s.resolve_image(results[i])
         download_links = s.resolve_download_links(
@@ -21,3 +20,6 @@ def hello_world(title):
     response = jsonify(message=results)
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
+
+
+@app.route("/d/<download_link>", methods=["GET"])
