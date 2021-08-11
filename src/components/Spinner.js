afc49571a73c63fa485eba49bedaf6fd254daebf
Amir <amirbrahamm@gmail.com>
2021-08-11 20:33:20 +0100

    wtf

--- src/components/Spinner.js
+++
@@ -1,4 +0,0 @@
-function Spinner() {
-    return <div className="w-full py-12 "> <div className="mx-auto loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-14 w-14"></div></div>
-}
-export default Spinner
